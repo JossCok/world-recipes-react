b31f764b5efc0efcf89f0ae8857f696ed4519114
Joss Cok <josipacok@gmail.com>
2026-02-24 09:15:38 -0600

    Initial commit: clean with .gitignore

---
+++ src/data/recipes.js
@@ -0,0 +1,46 @@
+export const cuisines = {
+  Italian: [
+    {
+      name: "Spaghetti Carbonara",
+      img: "https://www.themealdb.com/images/media/meals/llcbn01574260722.jpg",
+      description: "A creamy Roman pasta with eggs, cheese, pancetta, and pepper.",
+      ingredients: ["Spaghetti", "Eggs", "Pancetta", "Pecorino Romano", "Black Pepper"]
+    },
+    {
+      name: "Margherita Pizza",
+      img: "https://www.themealdb.com/images/media/meals/x0lk931587671540.jpg",
+      description: "Classic Neapolitan pizza with tomato, mozzarella, and basil.",
+      ingredients: ["Pizza dough", "Tomato sauce", "Mozzarella", "Basil"]
+    }
+  ],
+
+  Spanish: [
+    {
+      name: "Paella",
+      img: "https://www.themealdb.com/images/media/meals/1520081754.jpg",
+      description: "Traditional Valencian rice dish with seafood, saffron, and vegetables.",
+      ingredients: ["Rice", "Shrimp", "Mussels", "Saffron", "Peppers"]
+    },
+    {
+      name: "Tortilla Española",
+      img: "https://www.themealdb.com/images/media/meals/quuxsx1511476154.jpg",
+      description: "Spanish omelette made with potatoes and onions.",
+      ingredients: ["Eggs", "Potatoes", "Onions", "Olive oil"]
+    }
+  ],
+
+  Balkan: [
+    {
+      name: "Ćevapi",
+      img: "https://www.themealdb.com/images/media/meals/vvpprx1487325699.jpg",
+      description: "Grilled minced meat sausages, usually served with flatbread and onions.",
+      ingredients: ["Minced beef", "Garlic", "Salt", "Pepper"]
+    },
+    {
+      name: "Sarma",
+      img: "https://www.themealdb.com/images/media/meals/xxrxux1503070723.jpg",
+      description: "Cabbage rolls stuffed with minced meat and rice.",
+      ingredients: ["Cabbage leaves", "Minced meat", "Rice", "Spices"]
+    }
+  ]
+};
