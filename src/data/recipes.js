43a26c53fa4e7f705339045f8e969160630ff759
Joss Cok <josipacok@gmail.com>
2026-03-12 09:31:15 -0500

    Recipes photo update

--- src/data/recipes.js
+++ src/data/recipes.js
@@ -99,7 +99,7 @@ export const cuisines = {
 
 {
       name: "Gazpacho",
-      img: "https://images.unsplash.com/photo-1562967914-608f82629710",
+      img: "https://images.unsplash.com/photo-1654844828827-e93aa9298cdd?q=80&w=735&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       description: "Cold tomato soup blended with vegetables.",
       ingredients: [
         "Tomatoes",
